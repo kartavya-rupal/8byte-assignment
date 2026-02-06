2f11c7fcaaa0e587db387206c5f1b7555ac1350a
kartavya-rupal <kartavyarupal.kr@gmail.com>
2026-02-06 19:27:36 +0530

    added frontend env

--- frontend/services/portfolio.api.ts
+++ frontend/services/portfolio.api.ts
@@ -1,7 +1,7 @@
 import { PortfolioStock } from "@/types/porfolio";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/portfolio";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/portfolio";
 
 export const fetchPortfolio = async (): Promise<PortfolioStock[]> => {
     const response = await axios.get(API_URL);
